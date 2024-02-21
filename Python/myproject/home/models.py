3c785356cf9a69c5e8e8a3cf64e34ec78e7cb386
geekydwij <dwij@technmanconsulting.com>
2024-02-21 14:00:36 -0500

    session #2 - Models

--- Python/myproject/home/models.py
+++ Python/myproject/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    year = models.IntegerField()
+    color = models.CharField(max_length=20)
+    manufacturer = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    sold = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.color} {self.year} {self.manufacturer} {self.model}"
--- Python/myproject/myproject/settings.py
+++ Python/myproject/myproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': '2024',
+        'NAME': 'django',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -126,8 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
